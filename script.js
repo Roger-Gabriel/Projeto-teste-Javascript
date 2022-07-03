e99e60474569cf08e93fc7d5163d5aa8dbef98a0
RogerGabriel <rogergabriel975@gmail.com>
2022-07-03 17:34:16 -0300

    Estudo javascript

---
+++ script.js
@@ -0,0 +1,21 @@
+var animal = "Elefante";
+
+switch(animal){
+    case "Cavalo":
+        console.log(animal);
+        console.log('É um animal dócil e muito veloz !');
+        break;
+
+    case "Cachorro":
+        console.log(animal);
+        console.log('Dependendo da raça pode ser dócil ou agressivo!');
+        break;
+
+    case "Elefante":
+        console.log(animal);
+        console.log('É um animal pesado e muito grande!');
+        break;
+
+    default:
+        console.log('Não reconhecemos esse animal!')
+}
